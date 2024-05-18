80ef412184b285dd58e17a8cec7883494feee05c
github <164698300+githubkusumam@users.noreply.github.com>
2024-05-18 16:48:59 -0400

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -3,8 +3,8 @@ set -e
 
 # Pull the Docker image from Docker Hub
 echo "docker pull"
-docker pull himam29/pythonapp:latest
+docker pull kusumamvarghese/pythonapp
 
 # Run the Docker image as a container
 echo "docker run"
-docker run -d -p 5000:5000 himam29/pythonapp:latest
+docker run -d -p 5000:5000 kusumamvarghese/pythonapp
